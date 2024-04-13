33e39b7e1c1e04980717830c7d018d4e17a2e56f
yarahmadi0077 <yarahmadi0077@gmail.com>
2024-04-13 14:26:37 +0330

    1st commit by M.Yarahmadi add empty version of Voting.java .

--- src/VotingSystem.java
+++ src/VotingSystem.java
@@ -8,4 +8,32 @@
 
 public class VotingSystem {
 
+    private ArrayList<Voting> votingList ;
+
+    /**
+     * constructor for class VotingSystem
+     */
+    public VotingSystem() {
+        votingList = new ArrayList<Voting>();
+    }
+
+    /**
+     * Gets the VotingList
+     *
+     * @return votingList
+     */
+    public ArrayList<Voting> getVotingList() {
+        return votingList;
+    }
+
+    /**
+     * Gets the Voting
+     * use Voting.java
+     *
+     * @param index
+     * @return votingList
+     */
+    public Voting getVoting(int index) {
+        return votingList.get(index);
+    }
 }
