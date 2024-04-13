d49fcf64430deaa2ee9372a6fab54479a4b8788b
yarahmadi0077 <yarahmadi0077@gmail.com>
2024-04-13 14:20:31 +0330

    1st commit by M.Yarahmadi add empty version of Voting.java .

---
+++ src/VotingSystem.java
@@ -0,0 +1,11 @@
+/**
+ * Managing voting and results
+ * @author Mohammad Yarahmadi
+ * @author yarahmadi0077@aut.ac.ir
+ * @version 1.0
+ * @since 2024/04/13
+ */
+
+public class VotingSystem {
+
+}
