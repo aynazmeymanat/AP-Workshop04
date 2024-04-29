28ebeaf777cfcb96fe2b5fefe4b5a5008874adac
aynazmeymanat <aynazmeymanat@gmail.com>
2024-04-29 12:37:06 +0330

    first commit

---
+++ src/Vote.java
@@ -0,0 +1,52 @@
+import java.util.Objects;
+/**
+ * Voters details
+ * @author Aynaz Meymanat
+ * @author aynazmeymanat@aut.ac.ir
+ * @version 1.0.0
+ * @since 2024/04/14
+ */
+class Vote {
+    //fields
+    private final Person voter ;
+    private final String date  ;
+
+    /**
+     * constructor for class Vote
+     */
+    public Vote(Person voter, String date) {
+        this.voter = voter;
+        this.date = date;
+    }
+
+    /**
+     * Gets the voter
+     *
+     * @return voter
+     */
+    public Person getVoter() {
+        return voter;
+    }
+
+    /**
+     * Gets the Date
+     *
+     * @return Date
+     */
+    public String getDate() {
+        return date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vote vote = (Vote) o;
+        return Objects.equals(voter, vote.voter) && Objects.equals(date, vote.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(voter, date);
+    }
+}
