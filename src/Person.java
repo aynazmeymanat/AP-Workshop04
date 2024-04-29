5ce0a5e7ec14b4238aa24f6aa0b78fa683d7333b
aynazmeymanat <aynazmeymanat@gmail.com>
2024-04-29 12:47:35 +0330

    2st commit by A.Meymanat add new version off Person.java

---
+++ src/Person.java
@@ -0,0 +1,38 @@
+
+public class Person {
+    //fields
+    private String firstname;
+    private String lastname ;
+
+    /**
+     * constructor for class Person
+     */
+    public Person(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+    /**
+     * Gets the firstname
+     *
+     * @return firstname
+     */
+    public String getFirstname() {
+        return firstname;
+    }
+
+    /**
+     * Gets the lastname
+     *
+     * @return lastname
+     */
+    public String getLastname() {
+        return lastname;
+    }
+
+    /**
+     * @return person details as one string
+     */
+    public String toString(){
+        return firstname + " " + lastname ;
+    }
+}
